200b6b843c76181aaf51f28764cf8f51d0fa1ab6
TeclaGastaldi <tecla.gastaldi@studio.unibo.it>
2026-03-04 17:43:38 +0100

    Create 02_visualization.R

---
+++ Code/02_visualization.R
@@ -0,0 +1,45 @@
+#R code for visualizing satellite images
+
+library(terra)
+library(imageRy)
+
+install.packages("viridis")
+
+im.list()
+
+#importing images
+b2<-im.import("sentinel.dolomites.b2.tif")
+
+#Changing colors: digita su google r colors
+
+cl <- colorRampPalette(c("salmon","seagreen1","royalblue3"))(100)#la parentesi esterna vuole dare numero di sfumature tra un colore e l'altro
+plot(b2, col=cl)
+
+#oppure usare il package viridis, che ha legende già create visibili anche da daltonici
+
+library(viridis)
+plot(b2,col=inferno(100))
+
+#Excercise: plot with mako
+plot(b2,col=mako(100))
+
+#Excercise: plot band 2 with a greyscale palette
+
+cl.1<- colorRampPalette(c("gray41","#9C9C9C","gray81"))(100)
+plot(b2,col=cl.1)
+
+#Multiframe
+par(mfrow=c(1,2))
+plot(b2,col=cl.1)
+plot(b2,col=inferno(100))
+
+dev.off()
+
+?im.multiframe
+im.multiframe(1,2)
+plot(b2,col=cl.1)
+plot(b2,col=inferno(100))
+
+
+
+
