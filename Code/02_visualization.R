1650c8fa733a9684ad8350c1d5e8063070b21cb1
TeclaGastaldi <tecla.gastaldi@studio.unibo.it>
2026-03-05 14:55:06 +0100

    Update 02_visualization.R

--- Code/02_visualization.R
+++ Code/02_visualization.R
@@ -40,6 +40,29 @@ im.multiframe(1,2)
 plot(b2,col=cl.1)
 plot(b2,col=inferno(100))
 
+#Import band 4
+b3<-im.import("sentinel.dolomites.b3.tif")
+
+#Excerise: change viridis palette
+plot(b3,col=mako(100))
+plot(b3,col=plasma(100))
+
+#Import band 4
+b4<-im.import("sentinel.dolomites.b4.tif")
+
+#Import band 8
+b8<-im.import("sentinel.dolomites.b8.tif")
+
+#Excercise: multiframe with the 4 bands, legendes: in line with the wavelenght
+im.multiframe(2,2)
+plot(b2,col=mako(100))
+plot(b3,col=viridis(100))
+plot(b4,col=rocket(100))
+cl.2<- colorRampPalette(c("slateblue","mediumorchid2","lightpink"))(100)
+plot(b8,col=cl.2)
+
+
+
 
 
 
